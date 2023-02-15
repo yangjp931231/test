4ed5030a16664338143be8c20bd148a0302cde2b
admin <1573946275@qq.com>
2023-02-15 08:56:59 +0800

    提交到git

---
+++ src/main/java/com/smit/MySpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.smit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther:
+ * @Date:
+ * @Description:
+ * @version:1.0
+ */
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
